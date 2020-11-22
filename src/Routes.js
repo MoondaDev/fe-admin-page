031c2c99b3ffff28718cbab9659af32b38dd064f
김동주 <hepheir@gmail.com>
2020-11-22 19:02:28 +0900

    Register Login page to the Router

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import {
   Route,
   Switch
 } from "react-router-dom";
+import LoginPage from "./pages/Login/index";
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" />
+        <Route path="/login" component={LoginPage} />
       </Switch>
     </Router>
   );
