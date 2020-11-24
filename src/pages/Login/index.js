f7421efb765c702e9dde9b72551d273669aa2356
김동주 <hepheir@gmail.com>
2020-11-24 11:48:38 +0900

    #11 - Replace DOM style from `function` -> `React.Component`

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,40 +1,45 @@
+import React from "react";
 
-export default function LoginPage() {
-  return (
-    <div className="center">
-      <img
-        className="logo"
-        alt="Moonda logo with title"
-        src="logo-with-title.png"
-      />
-      <form>
-        <div className="login-box">
-          <div className="login-input-container">
-            <div className="login-input-row">
-              <label className="login-input-label">ID</label>
-              <input
-                className="login-input-input"
-                type="text"
-                placeholder="Enter your ID"
-              />
+class LoginPage extends React.Component {
+  render() {
+    return (
+      <div className="center">
+        <img
+          className="logo"
+          alt="Moonda logo with title"
+          src="logo-with-title.png"
+        />
+        <form>
+          <div className="login-box">
+            <div className="login-input-container">
+              <div className="login-input-row">
+                <label className="login-input-label">ID</label>
+                <input
+                  className="login-input-input"
+                  type="text"
+                  placeholder="Enter your ID"
+                />
+              </div>
+              <div className="login-input-row">
+                <label className="login-input-label">Password</label>
+                <input
+                  className="login-input-input"
+                  type="password"
+                  placeholder="Enter your password"
+                />
+              </div>
             </div>
-            <div className="login-input-row">
-              <label className="login-input-label">Password</label>
-              <input
-                className="login-input-input"
-                type="password"
-                placeholder="Enter your password"
-              />
-            </div>
+            <button
+              className="login-button"
+              type="submit"
+            >
+              Log in
+            </button>
           </div>
-          <button
-            className="login-button"
-            type="submit"
-          >
-            Log in
-          </button>
-        </div>
-      </form>
-    </div>
-  )
+        </form>
+      </div>
+    );
+  }
 }
+
+export default LoginPage;
