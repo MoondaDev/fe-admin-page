a3ed87fbd332cf4df702f935a534bc85337c9310
김동주 <hepheir@gmail.com>
2020-11-24 10:59:50 +0900

    Delete App.js / index.js now directly accesses Router.js
    App.js가 생각보다 쓸모가 없어보여 삭제했습니다.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './scripts/reportWebVitals';
+import Routes from './Routes';
 import "./styles/default.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
