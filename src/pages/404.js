bbf29a88dbf760c4bc34ebdaa07c1cb30b860ef3
김동주 <hepheir@gmail.com>
2020-11-25 14:15:29 +0900

    Modularize Empty page component as "src/pages/404.js"
    빈 페이지에 해당하는 컴포넌트를 admin/index.js 로 부터 분리하였습니다.

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Empty extends React.Component {
+  render() {
+    return (
+      <>
+        404 Page not found
+      </>
+    );
+  }
+}
+
+export default Empty;
