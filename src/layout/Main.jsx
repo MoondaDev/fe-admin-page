754b0e115e0abd22f73aad77aa69b8d14d006ed0
김동주 <hepheir@gmail.com>
2020-12-05 02:00:08 +0900

    Fix #18 for "layout/Main.jsx"

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+import { HEADER_HEIGHT } from "./Header";
+import { SIDEBAR_WIDTH } from "./Sidebar";
+
+const Layout = styled.main`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+  padding-top: ${HEADER_HEIGHT};
+  padding-left: ${SIDEBAR_WIDTH};
+`;
+
+const Wrapper = styled.div`
+  padding: 0rem 1rem;
+`;
 
 class Main extends React.Component {
   render() {
-    const { children } = this.props;
-
     return (
-      <main className="layout">
-        <div className="main-wrapper">
-          {children}
-        </div>
-      </main>
+      <Layout>
+        <Wrapper>{this.props.children}</Wrapper>
+      </Layout>
     );
   }
 }
