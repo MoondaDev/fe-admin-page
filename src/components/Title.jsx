d79acd84bb7105f114560bf437fa9c360c3f4a68
Hepheir <hepheir@gmail.com>
2020-12-09 06:19:33 +0900

    Remove padding-bottom from Title (padding-left as well)

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const Heading = styled.h1`
-  padding: 1rem;
+  padding-top: 1rem;
+  padding-left: 1rem;
   font-size: 1.6rem;
 `;
 
