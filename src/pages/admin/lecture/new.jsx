f6aec95e6c3895aceb2257eb4d85ca550502271f
Hepheir <hepheir@gmail.com>
2020-12-11 23:46:43 +0900

    Use Switch / Route before adding Modal window

--- src/pages/admin/lecture/new.jsx
+++ src/pages/admin/lecture/new.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 import Header from "../../../layout/Header";
 import Sidebar from "../../../layout/Sidebar";
@@ -11,6 +16,7 @@ import { LargeTable } from "../../../components/Table";
 import { GrayButton, BlueButton, GreenButton } from "../../../components/Button";
 
 const PATHNAME = '/admin/lecture/new';
+const MODAL_PATHNAME = PATHNAME+'/manual';
 
 class Page extends React.Component {
   render() {
@@ -18,98 +24,113 @@ class Page extends React.Component {
       <>
         <Header/>
         <Sidebar/>
-        <Main>
-          <RowComponentGroup>
-            <Title>강좌 정보 추가</Title>
-          </RowComponentGroup>
+        <Switch>
+          <Route path={MODAL_PATHNAME}>
+            <Modal>
 
-          <RowComponentGroup>
-            <RightComponentGroup>
-              <GrayButton>파일 내보내기</GrayButton>
-              <BlueButton>저장</BlueButton>
-            </RightComponentGroup>
-          </RowComponentGroup>
+             </Modal>
+          </Route>
 
-          <RowComponentGroup>
-            <BoxForm>
-              <BoxFormRow>
-                <label>문화센터</label>
-                <input type="text" />
-              </BoxFormRow>
-              <BoxFormRow>
-                <label>학기 분류</label>
-                <select>
-                  <option value="">2021 봄학기</option>
-                  <option value="">2021 여름학기</option>
-                  <option value="">2021 가을학기</option>
-                  <option value="">2021 겨울학기</option>
-                </select>
-              </BoxFormRow>
-              <BoxFormRow>
-                <label>활성</label>
-                <input type="checkbox" />
-              </BoxFormRow>
-              <BoxFormRow>
-                <label>강좌 엑셀파일</label>
-                <input type="file" />
-              </BoxFormRow>
-              <BoxFormRow>
-                <label>담당자</label>
-                <input type="text" />
-              </BoxFormRow>
-            </BoxForm>
-          </RowComponentGroup>
+          <Route path="*">
+            <Main>
+              <RowComponentGroup>
+                <Title>강좌 정보 추가</Title>
+              </RowComponentGroup>
 
-          <RowComponentGroup>
-            <LeftComponentGroup>
-              <SearchBar>강좌 검색</SearchBar>
-            </LeftComponentGroup>
-            <RightComponentGroup>
-              <GreenButton>강좌 추가</GreenButton>
-            </RightComponentGroup>
-          </RowComponentGroup>
+              <RowComponentGroup>
+                <LeftComponentGroup>
+                  <RedButton>삭제하기</RedButton>
+                </LeftComponentGroup>
+                <RightComponentGroup>
+                  <GrayButton>파일 내보내기</GrayButton>
+                  <BlueButton>저장</BlueButton>
+                </RightComponentGroup>
+              </RowComponentGroup>
 
-          <RowComponentGroup>
-            <LargeTable>
-              <thead>
-                <tr>
-                  <th><input type="checkbox" /></th>
-                  <th>추천</th>
-                  <th>번호</th>
-                  <th>강좌명</th>
-                  <th>대상</th>
-                  <th>카테고리</th>
-                  <th>장소</th>
-                  <th>강좌 시간</th>
-                  <th>수강 기간</th>
-                  <th>등록 기간</th>
-                  <th>수강료</th>
-                  <th>정원</th>
-                  <th>안내사항1</th>
-                  <th>안내사항2</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td><input type="checkbox" /></td>
-                  <td><input type="checkbox" /></td>
-                  <td>1234</td>
-                  <td>웹개발</td>
-                  <td>나</td>
-                  <td>프로그래밍</td>
-                  <td>집</td>
-                  <td>항상</td>
-                  <td>12/11까지</td>
-                  <td>N/A</td>
-                  <td>0 won</td>
-                  <td>2</td>
-                  <td>생각보다</td>
-                  <td>어렵네요 Oops</td>
-                </tr>
-              </tbody>
-            </LargeTable>
-          </RowComponentGroup>
-        </Main>
+              <RowComponentGroup>
+                <BoxForm>
+                  <BoxFormRow>
+                    <label>문화센터</label>
+                    <input type="text" />
+                  </BoxFormRow>
+                  <BoxFormRow>
+                    <label>학기 분류</label>
+                    <select>
+                      <option value="">2021 봄학기</option>
+                      <option value="">2021 여름학기</option>
+                      <option value="">2021 가을학기</option>
+                      <option value="">2021 겨울학기</option>
+                    </select>
+                  </BoxFormRow>
+                  <BoxFormRow>
+                    <label>활성</label>
+                    <input type="checkbox" />
+                  </BoxFormRow>
+                  <BoxFormRow>
+                    <label>강좌 엑셀파일</label>
+                    <input type="file" />
+                  </BoxFormRow>
+                  <BoxFormRow>
+                    <label>담당자</label>
+                    <input type="text" />
+                  </BoxFormRow>
+                </BoxForm>
+              </RowComponentGroup>
+
+              <RowComponentGroup>
+                <LeftComponentGroup>
+                  <SearchBar>강좌 검색</SearchBar>
+                </LeftComponentGroup>
+                <RightComponentGroup>
+                  <Link to={MODAL_PATHNAME}>
+                    <GreenButton>강좌 추가</GreenButton>
+                  </Link>
+                </RightComponentGroup>
+              </RowComponentGroup>
+
+              <RowComponentGroup>
+                <LargeTable>
+                  <thead>
+                    <tr>
+                      <th><input type="checkbox" /></th>
+                      <th>추천</th>
+                      <th>번호</th>
+                      <th>강좌명</th>
+                      <th>대상</th>
+                      <th>카테고리</th>
+                      <th>장소</th>
+                      <th>강좌 시간</th>
+                      <th>수강 기간</th>
+                      <th>등록 기간</th>
+                      <th>수강료</th>
+                      <th>정원</th>
+                      <th>안내사항1</th>
+                      <th>안내사항2</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td><input type="checkbox" /></td>
+                      <td><input type="checkbox" /></td>
+                      <td>1234</td>
+                      <td>웹개발</td>
+                      <td>나</td>
+                      <td>프로그래밍</td>
+                      <td>집</td>
+                      <td>항상</td>
+                      <td>12/11까지</td>
+                      <td>N/A</td>
+                      <td>0 won</td>
+                      <td>2</td>
+                      <td>생각보다</td>
+                      <td>어렵네요 Oops</td>
+                    </tr>
+                  </tbody>
+                </LargeTable>
+              </RowComponentGroup>
+            </Main>
+          </Route>
+        </Switch>
       </>
     );
   }
