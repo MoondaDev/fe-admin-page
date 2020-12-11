44a8149ba18f2d71edab61d07408ca1a8e182ecb
Hepheir <hepheir@gmail.com>
2020-12-11 23:28:48 +0900

    Divide `Main` into a Component with 2 elements : `Wrapper` and `Container`

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
+import React from "react";
 import { HEADER_HEIGHT } from "./Header";
 import { SIDEBAR_WIDTH } from "./Sidebar";
 
-const Main = styled.main`
+const Wrapper = styled.main`
   position: absolute;
   top: 0;
   left: 0;
@@ -11,6 +12,13 @@ const Main = styled.main`
   height: 100%;
   padding-top: ${HEADER_HEIGHT};
   padding-left: ${SIDEBAR_WIDTH};
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  background-color: white;
 `;
 
 const RowComponentGroup = styled.div`
@@ -28,6 +36,18 @@ const LeftComponentGroup = styled.div`
 const RightComponentGroup = styled.div`
   margin-left: auto;
 `;
+
+class Main extends React.Component {
+  render() {
+    return (
+      <Wrapper>
+        <Container>
+          {this.props.children}
+        </Container>
+      </Wrapper>
+    );
+  }
+}
 
 export {
   Main as default,
